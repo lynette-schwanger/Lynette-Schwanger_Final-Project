e4fd68ba64b98a2f3a515018ddfcdf9f938253cf
Lynette Schwanger <lynette.schwanger@gmail.com>
2025-12-05 09:54:17 -0700

    v1

--- Schwanger_Final_Project/app.R
+++ Schwanger_Final_Project/app.R
@@ -2,11 +2,15 @@
 
 knitr::opts_chunk$set(echo = TRUE)
 
-source("setup.R")
+library(shiny)
+library(leaflet)
+library(sf)
+library(dplyr)
+library(tigris)
 
-usfws_co_wetlands <- "/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/data/CO_geopackage_wetlands.gpkg"
+usfws_co_wetlands <- st_read("/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/data/CO_geopackage_wetlands.gpkg")
 
-cdot_fens <- "/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/data/Potential_Fen_Wetlands.geojson"
+cdot_fens <- st_read("/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/data/Potential_Fen_Wetlands.geojson")
 
 # Run the application 
 shinyApp(ui = ui, server = server)

11533f7fc671e0c006a5652f35689d83cc724cbe
Lynette Schwanger <lynette.schwanger@gmail.com>
2025-12-08 09:51:50 -0700

    v2 - need to transform and crop cnhp_fens

--- Schwanger_Final_Project/app.R
+++ Schwanger_Final_Project/app.R
@@ -1,16 +1,49 @@
 
-
-knitr::opts_chunk$set(echo = TRUE)
-
 library(shiny)
 library(leaflet)
 library(sf)
 library(dplyr)
 library(tigris)
 
-usfws_co_wetlands <- st_read("/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/data/CO_geopackage_wetlands.gpkg")
+load("fenAppData.RData")
 
-cdot_fens <- st_read("/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/data/Potential_Fen_Wetlands.geojson")
+ui <- fluidPage(
+  titlePanel("Lake & Eagle County Fen & Wetland Explorer"),
+  leafletOutput("map", height = "80vh")
+)
+
+server <- function(input, output, session) {
+  
+  output$map <- renderLeaflet({
+    leaflet() |>
+      addProviderTiles("Esri.WorldTopoMap") |>
+      addPolygons(
+        data = lake_eagle,
+        fill = FALSE,
+        weight = 2,
+        color = "black",
+        group = "Counties"
+      ) |>
+      addPolygons(
+        data = usfws_co_wetlands_crop,
+        weight = 1,
+        color = "blue",
+        fillOpacity = 0.4,
+        group = "Wetlands"
+      ) |>
+      addPolygons(
+        data = cdot_fens_crop,
+        weight = 1,
+        color = "green",
+        fillOpacity = 0.7,
+        group = "Potential fens"
+      ) |>
+      addLayersControl(
+        overlayGroups = c("Counties", "Wetlands", "Potential fens"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
--- Schwanger_Final_Project/fen_wetland_data.R
+++ Schwanger_Final_Project/fen_wetland_data.R
@@ -1,6 +1,6 @@
-# prep data
 
 library(sf)
+sf_use_s2(FALSE)
 library(dplyr)
 library(tigris)
 
@@ -8,17 +8,34 @@ usfws_co_wetlands <- st_read("/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/
 
 cdot_fens <- st_read("/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/data/Potential_Fen_Wetlands.geojson")
 
+cnhp_fens <- st_read("https://cnhp.colostate.edu/arcgis/rest/services/Wetland_Inv/Fen_Mapping/MapServer/0/query?where=1=1&outFields=*&f=geojson")
+
 co_counties <- counties(state = "CO")
+
+lake_eagle <- co_counties %>% 
+  filter(NAME %in% c("Eagle", "Lake"))
+
+lake_eagle_water <- linear_water(
+  state  = "CO",
+  county = c("Lake", "Eagle"))
 
 target_crs <- "EPSG:4326"
 
 usfws_co_wetlands <- st_transform(usfws_co_wetlands, target_crs)
 
-cdot_fens <- st_transform(cdot_fens,target_crs)   
+cdot_fens <- st_transform(cdot_fens,target_crs)  
+
+lake_eagle <- st_transform(lake_eagle,target_crs)
+
+lake_eagle_water <- st_transform(lake_eagle_water,target_crs)
 
 co_counties <- co_counties %>%
   st_as_sf() %>%
   st_transform(target_crs)
 
-lake_eagle_co <- co_counties %>%
+usfws_co_wetlands_crop <- st_intersection(usfws_co_wetlands, lake_eagle)
+
+cdot_fens_crop <- st_intersection(cdot_fens, lake_eagle)
+
+save(usfws_co_wetlands_crop, cdot_fens_crop, lake_eagle, lake_eagle_water, file = "fenAppData.RData")
   

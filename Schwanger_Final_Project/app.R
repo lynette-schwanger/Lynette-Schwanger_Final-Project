21e898aebc2cc0127ed8a71fdd3e30c54d75fbac
Lynette Schwanger <lynette.schwanger@gmail.com>
2025-12-12 09:35:25 -0700

    v6 - add data citations, popup info, maybe beaver habitat?

--- Schwanger_Final_Project/app.R
+++ Schwanger_Final_Project/app.R
@@ -14,7 +14,6 @@ ui <- fluidPage(
     tags$li(strong("USFWS – Wetlands:"), "National Wetlands Inventory polygons."),
     tags$li(strong("CDOT – Potential Fens:"), "potential fens mapped by the Colorado Department of Transportation along transportation corridors."),
     tags$li(strong("CNHP – Potential Fens:"), "potential fens mapped by the Colorado Natural Heritage Program.")),
-  p("The limited overlap between layers highlights how agency missions and survey designs shape where wetlands and fens have been mapped so far."),
   leafletOutput("map", height = "75vh"))
 
 server <- function(input, output, session) {
@@ -27,29 +26,25 @@ server <- function(input, output, session) {
         data = eagle_co,
         fill = FALSE,
         weight = 2,
-        color = "black"
-      ) %>%
+        color = "black") %>%
       addPolygons(
         data = usfws_co_wetlands_crop,
         weight = 1,
         color = "purple",
-        fillOpacity = 0.4,
-        group = "USFWS - Wetlands"
-      ) %>%
+        fillOpacity = 1,
+        group = "USFWS - Wetlands") %>%
       addPolygons(
         data = cdot_fens_crop,
         weight = 1,
         color = "orange",
-        fillOpacity = 0.7,
-        group = "CDOT - Potential Fens"
-      ) %>%
+        fillOpacity = 1,
+        group = "CDOT - Potential Fens") %>%
       addPolygons(
         data = cnhp_fens_crop,
         weight = 1,
         color = "darkgreen",
-        fillOpacity = 0.6,
-        group = "CNHP - Potential Fens"
-      ) %>%
+        fillOpacity = 1,
+        group = "CNHP - Potential Fens") %>%
       addLegend(
         position = "topright",
         title    = "Data Source",
@@ -57,13 +52,17 @@ server <- function(input, output, session) {
         labels   = c("USFWS - Wetlands",
                      "CDOT - Potential Fens",
                      "CNHP - Potential Fens"),
-        opacity  = 1
-      ) %>%
+        opacity  = 1) %>%
     addLayersControl(
       overlayGroups = c("USFWS - Wetlands",
                         "CDOT - Potential Fens",
                         "CNHP - Potential Fens"),
-      options = layersControlOptions(collapsed = FALSE))
+      options = layersControlOptions(collapsed = FALSE)) %>%
+      addPolylines(
+        data  = eagle_water_crop,
+        color = "midnightblue",
+        weight = .6,
+        fillOpacity = .3)
   })
 }
 
--- Schwanger_Final_Project/fen_wetland_data.R
+++ Schwanger_Final_Project/fen_wetland_data.R
@@ -15,6 +15,8 @@ co_counties <- counties(state = "CO")
 eagle_co <- co_counties %>% 
   filter(NAME == "Eagle")
 
+eagle_water <- linear_water(state  = "CO", county = "Eagle")
+
 target_crs <- "EPSG:4326"
 
 usfws_co_wetlands <- st_transform(usfws_co_wetlands, target_crs)
@@ -25,12 +27,16 @@ cnhp_fens <- st_transform(cnhp_fens, target_crs)
 
 eagle_co <- st_transform(eagle_co,target_crs)
 
+eagle_water <- st_transform(eagle_water,target_crs)
+
 usfws_co_wetlands_crop <- st_intersection(usfws_co_wetlands, eagle_co)
 
 cdot_fens_crop <- st_intersection(cdot_fens, eagle_co)
 
 cnhp_fens_crop <- st_intersection(cnhp_fens, eagle_co)
 
-save(usfws_co_wetlands_crop, cdot_fens_crop, cnhp_fens_crop, eagle_co, file = "/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/Schwanger_Final_Project/fenAppData.RData")
+eagle_water_crop <- st_intersection(eagle_water, eagle_co)
+
+save(usfws_co_wetlands_crop, cdot_fens_crop, cnhp_fens_crop, eagle_co, eagle_water_crop, file = "/Users/lynetteschwanger/Desktop/Env-Data-Sci_2025/Lynette-Schwanger_Final-Project/Schwanger_Final_Project/fenAppData.RData")
   
 
